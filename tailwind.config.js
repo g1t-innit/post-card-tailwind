59d6a72ab944a97e489eb61fd41e6aeebd35b7dd
Reza ZadDadash <reza.zdsh@gmail.com>
2025-01-21 17:15:44 +0330

    fix: whole design fix/fonts, colors, and more styles fixes/design complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+      fontFamily: {
+          "josefin-sans-regular": ["JosefinSans-Regular", "sans-serif"],
+          "josefin-sans-bold": ["JosefinSans-Bold", "sans-serif"]
+      },
+      extend: {}
   },
   plugins: [],
-}
-
+};
